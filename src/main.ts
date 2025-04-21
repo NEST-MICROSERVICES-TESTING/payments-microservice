452322dc57ac11d6b4eadeb8e232127320ed3462
imb-bestrada <bstrada4@gmail.com>
2025-04-21 13:46:43 -0500

    UPD 21-4-25

--- src/main.ts
+++ src/main.ts
@@ -19,14 +19,15 @@ async function bootstrap() {
         })
     );
 
-    app.connectMicroservice<MicroserviceOptions>({
-        transport: Transport.NATS
-        ,options: {
-            servers: envs.natsServers
-        }
-    },{
-        inheritAppConfig: true 
-    });
+    app.connectMicroservice<MicroserviceOptions>(
+        {
+            transport: Transport.NATS
+            ,options: {
+                servers: envs.natsServers
+            }
+        },
+        { inheritAppConfig: true }
+    );
 
     await app.startAllMicroservices();
 
